8b800fe72ef2d8583ed4b4715e845d9437aab9aa
“Brenno <brennoeudes@gmail.com>
2023-06-07 11:39:05 -0300

    First page load JS adjust

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function toggleMode() {
-  // 1º instrução: pegar html
-  const html = document.documentElement; // document é a representação do documento em formato de objeto JS. Através do ponto "." acessamos a tag html através do documentElement.
+  const html = document.documentElement;
+  const isFirstLoad = !localStorage.getItem('modeToggled');
 
-  // 2º instrução: troca de classes
-  html.classList.toggle("light"); // método JS já pronto que simplifica a condicional comentada abaixo.
+  if (isFirstLoad && html.classList.contains('light')) {
+    const downloadButton = document.querySelector("ul");
+    const supporter1 = document.querySelector("#supporter-1");
+    downloadButton.style.display = "none";
+    supporter1.style.display = "none";
+  }
 
-  /* if(html.classList.contains('light')) {
-      html.classList.remove('light');
-    } else {
-      html.classList.add('light');
-    } */
+  html.classList.toggle("light");
+  localStorage.setItem('modeToggled', true);
 
-  // 3º instrução: pegar tags
-  const img = document.querySelector("#profile img"); // msm do CSS
+  const img = document.querySelector("#profile img");
   const h1 = document.querySelector("#profile h1");
   const text1 = document.querySelector("#text-1 p");
   const text2 = document.querySelector("#text-2 p");
   const downloadButton = document.querySelector("ul");
   const supporter1 = document.querySelector("#supporter-1");
 
-  // 4º instrução: substituir img e texto
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png"); // se light, add image light
+    img.setAttribute("src", "./assets/avatar-light.png");
     img.setAttribute(
       "alt",
       "Martin Luther King Jr. public speech and clenched fist."
@@ -32,7 +31,7 @@ function toggleMode() {
     downloadButton.style.display = "none";
     supporter1.style.display = "none";
   } else {
-    img.setAttribute("src", "./assets/avatar-dark.png"); // sem light, add image dark
+    img.setAttribute("src", "./assets/avatar-dark.png");
     img.setAttribute("alt", "Vinicius Jr fist raised with hand closed.");
     h1.textContent = "...someday, we all will play against racism!";
     text1.textContent = "It is not enough to say no racism. Be anti-racist!";
