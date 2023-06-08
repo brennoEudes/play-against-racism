4dc08551379888c6dce3477acd5e9544abcc38a8
“Brenno <brennoeudes@gmail.com>
2023-06-08 16:44:44 -0300

    profile img size adjust

--- script.js
+++ script.js
@@ -16,10 +16,12 @@ function toggleMode() {
   const h1 = document.querySelector("#profile h1");
   const text1 = document.querySelector("#text-1 p");
   const text2 = document.querySelector("#text-2 p");
-  const downloadButton = document.querySelector("ul");
+  const mainButton1 = document.querySelector("ul");
+  const mainButton2 = document.querySelector("ul li a");
   const supporter1 = document.querySelector("#supporter-1");
+  const supporter2 = document.querySelector("#supporter-1 img");
 
-  // 4º instrução: substituir img e texto
+  // 4º instrução: substituir img e textos
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png"); // se light, add image light
     img.setAttribute(
@@ -29,7 +31,7 @@ function toggleMode() {
     h1.textContent = "I have a dream...";
     text1.textContent = "";
     text2.textContent = "";
-    downloadButton.style.display = "none";
+    mainButton1.style.display = "none";
     supporter1.style.display = "none";
   } else {
     img.setAttribute("src", "./assets/avatar-dark.png"); // sem light, add image dark
@@ -37,7 +39,9 @@ function toggleMode() {
     h1.textContent = "...someday, we all will play against racism!";
     text1.textContent = "It is not enough to say no racism. Be anti-racist!";
     text2.textContent = "Click on the button below and play with us!";
-    downloadButton.style.display = "block";
+    mainButton1.style.display = "block";
+    mainButton2.setAttribute("src", "");
     supporter1.style.display = "block";
+    supporter2.setAttribute("src", "./assets/vini-jr-foundation-logo.svg");
   }
 }
